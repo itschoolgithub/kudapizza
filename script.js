2cd64fb42acf85e58b376e920cb43ac1f8cc940c
ITSchool <itschoolgithub@mail.ru>
2025-11-30 12:02:02 +0500

    popup

--- script.js
+++ script.js
@@ -26,3 +26,32 @@ document.querySelector('.info__link').addEventListener('click', function () {
     this.textContent = buttonText2;
     buttonText2 =  buttonText;
 })
+
+// ждем полной загрузки страницы
+document.addEventListener('DOMContentLoaded', function () {
+    const popupFilter = document.querySelector('.popup-filter');
+
+    if (popupFilter) {
+        const buttonPopup = document.querySelector('.catalog__filter');
+        if (buttonPopup) {
+            buttonPopup.addEventListener('click', function () {
+                document.body.classList.add('no-scroll');
+                popupFilter.classList.add('popup-filter--show')
+                setTimeout(function () {
+                    popupFilter.classList.add('popup-filter--active')
+                }, 10);
+            })
+        }
+
+        const buttonPopupClose = document.querySelector('.popup-filter__close');
+        if (buttonPopupClose) {
+            buttonPopupClose.addEventListener('click', function () {
+                popupFilter.classList.remove('popup-filter--active')
+                setTimeout(function () {
+                    popupFilter.classList.remove('popup-filter--show')
+                    document.body.classList.remove('no-scroll');
+                }, 300);
+            });
+        }
+    }
+});
